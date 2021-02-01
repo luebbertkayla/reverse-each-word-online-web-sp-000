987b2ff4f902dc904cbf33fb97f1d62291119c14
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-01 22:36:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_array = "Hello there, and how are you?".split
-  new_array.each do |word|
-    return new_array.reverse
+  new_array.each do |word|.reverse
+    return new_array
   end
 end
