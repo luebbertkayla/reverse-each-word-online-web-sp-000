99fcd58d08f74dbee141f80b72ae0a3d949c8d53
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-01 22:40:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each do {|word| word.reverse!}
+  reversed_array = new_array.each {|word| word.reverse!}
     return reversed_array.join(" ")
-  end
 end
